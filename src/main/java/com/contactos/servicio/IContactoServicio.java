b0b18a9dffbb7bbb515c1f7467fbc3b317a97d30
Samuel <samuel.iza@outlook.com>
2025-07-09 14:05:03 -0600

    Se agregan metodos a interfaz IContactoServicio

--- src/main/java/com/contactos/servicio/IContactoServicio.java
+++ src/main/java/com/contactos/servicio/IContactoServicio.java
@@ -1,4 +1,15 @@
 package com.contactos.servicio;
 
+import com.contactos.modelo.Contacto;
+
+import java.util.List;
+
 public interface IContactoServicio {
+    public List<Contacto> listarContactos();
+
+    public Contacto buscarContactoPorId(Integer idContacto);
+
+    public void guardarContacto(Contacto contacto);
+
+    public void eliminarContacto(Contacto contacto);
 }
